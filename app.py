f5d040ef678af0a623923b1b7f2f881414ed3334
ShubhamZoro <shubhamshekhar19@gmail.com>
2026-04-18 19:04:39 +0530

    creating app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="AI Attendence", page_icon="🤖")
+    st.title("AI Attendence")
+    st.write("AI Attendence")
+
+if __name__ == "__main__":
+    main()
