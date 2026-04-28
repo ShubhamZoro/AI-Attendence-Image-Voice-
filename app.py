2c8aac96752d13a0f750d63cc67d1627d5cfe2b5
ShubhamZoro <shubhamshekhar19@gmail.com>
2026-04-28 17:43:37 +0530

    update student screen, created subject share functionality and auto enrollment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.screens.home_screen import home_screen
 from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
+from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
     if 'login_type' not in st.session_state:
@@ -14,9 +15,16 @@ def main():
             teacher_screen()
         case 'student':
             student_screen()
-        case _:
-            st.error("Invalid login type")
+        
 
+    join_code = st.query_params.get('join-code')
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            st.header(f"{join_code}")
+            auto_enroll_dialog(join_code)
 
 
 if __name__ == "__main__":
--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -8,11 +8,77 @@ from PIL import Image
 import numpy as np
 from src.pipelines.face_pipeline import predict_attendance,get_face_embeddings
 from src.pipelines.voice_pipeline import get_voice_embedding
-from src.database.db import get_all_students,create_student
-
+from src.database.db import get_all_students,create_student,get_student_subjects,get_student_attendance,unenroll_student_to_subject
+from src.components.dialog_enroll import enroll_dialog
+from src.components.subject_card import subject_card
 def student_dashboard():
-    st.header(f"Welcome {st.session_state['student_data']['name']}",text_alignment='center')
+    student_data=st.session_state['student_data']
+    student_id=student_data['student_id']
+    c1,c2=st.columns(2,vertical_alignment="center",gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        st.subheader(f"Welcome {student_data['name']}",text_alignment='center')
+        if st.button("Logout",icon=':material/logout:',type='primary',width='stretch',key='loginbackbtn',shortcut="control+backspace"):
+            st.session_state.is_logged_in=False
+            del st.session_state.student_data
+            st.rerun()
+    st.space()
+    c1,c2=st.columns(2)
+    with c1:
+        st.header("Your Enrolled subjects")
+    with c2:
+        if st.button("Enroll in subject",type='primary',width='stretch'):
+            enroll_dialog()
     
+    st.divider()
+
+    with st.spinner("Loading your enrolled subjects.."):
+        subjects=get_student_subjects(student_id)
+        logs=get_student_attendance(student_id)
+    
+    stats_map = {}
+
+    for log in logs:
+        sid = log['subject_id']
+
+        if sid not in stats_map:
+            stats_map[sid] = {"total":0, "attended": 0}
+
+        stats_map[sid]['total'] +=1
+
+        if log.get('is_present'):
+            stats_map[sid]['attended'] += 1
+
+
+    cols = st.columns(2)
+    for i, sub_node in enumerate(subjects):
+        sub = sub_node['subjects']
+        sid = sub['subject_id']
+
+
+        stats = stats_map.get(sid,{"total":0, "attended": 0} )
+        def unenroll_button():
+                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+                    unenroll_student_to_subject(student_id, sid)
+                    st.toast(f"Unenrolled from {sub['name']} successfully!")
+                    st.rerun()
+
+        with cols[i % 2]:
+
+            subject_card(
+                name = sub['name'],
+                code =sub['subject_code'],
+                section = sub['section'],
+                stats = [
+                    ('📅', 'Total', stats['total']),
+                    ('✅', 'Attended', stats['attended']),
+                ],
+                footer_callback=unenroll_button
+            )
+
+            
+    footer_dashboard()
 
 
 def student_screen():
@@ -49,7 +115,7 @@ def student_screen():
                     all_students=get_all_students()
                     student=next((s for s in all_students if s['student_id']==student_id),None)
                     if student:
-                        st.session_state.is_loged_in=True
+                        st.session_state.is_logged_in=True
                         st.session_state.user_role='student'
                         st.session_state.student_data=student
                         st.toast(f"Welcome Back {student['name']}")
@@ -86,7 +152,7 @@ def student_screen():
                             response_data=create_student(new_name,face_emb,voice_emb)
                             if response_data:
                                 train_classifier()
-                                st.session_state.is_loged_in=True
+                                st.session_state.is_logged_in=True
                                 st.session_state.user_role='student'
                                 st.session_state.student_data=response_data[0]
                                 st.toast(f"Profile Created! Hi {new_name}")
